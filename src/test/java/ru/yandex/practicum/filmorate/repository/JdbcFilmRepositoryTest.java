1b4097efa96b831c8373dccffcab9142ab556a3f
vasilgmkz <vasilgmkz@yandex.kz>
2024-08-24 16:42:35 +0300

    add-database version_3 (tests) with non-working tests

--- src/test/java/ru/yandex/practicum/filmorate/repository/JdbcFilmRepositoryTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/repository/JdbcFilmRepositoryTest.java
@@ -1,15 +1,34 @@
 package ru.yandex.practicum.filmorate.repository;
-
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.DisplayName;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.context.annotation.Import;
-
-@JdbcTest
-@Import(JdbcFilmRepository.class)
-@RequiredArgsConstructor (onConstructor_ = @Autowired)
-@DisplayName("JdbcFilmRepository")
-class JdbcFilmRepositoryTest {
-
-}
+//@JdbcTest
+//@Import(JdbcFilmRepository.class)
+//@RequiredArgsConstructor(onConstructor_ = @Autowired)
+//@DisplayName("JdbcFilmRepository")
+//class JdbcFilmRepositoryTest {
+//    public static final long TEST_FILM_ID = 1L;
+//    private final JdbcFilmRepository jdbcFilmRepository;
+//
+//    static Film getTestFilm() {
+//        Film film = new Film();
+//        Genre genre = new Genre();
+//        genre.setId(1);
+//        genre.setName("Боевик");
+//        Rating rating = new Rating();
+//        rating.setId(1);
+//        rating.setName("G");
+//        film.setId(TEST_FILM_ID);
+//        film.setName("name");
+//        film.setDescription("description");
+//        film.setDuration(200);
+//        film.setReleaseDate(LocalDate.of(2022, 4, 12));
+//        film.getGenres().add(genre);
+//        film.setMpa(rating);
+//        return film;
+//    }
+//
+//    @Test
+//    @DisplayName("должен находить фильм по ид")
+//    public void should_return_film_when_find_by_id() {
+//        Film film = jdbcFilmRepository.getFilmId(TEST_FILM_ID);
+//        assertThat(film).usingRecursiveComparison().isEqualTo(getTestFilm());
+//    }
+//}
--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -1,3 +1,22 @@
 insert into USERS (email, login, user_name, birthday)
 values ('email@email.com', 'user', 'test', '2000-03-22');
 
+INSERT INTO GENRES (genre_name)
+VALUES ('Боевик');
+
+INSERT INTO Rating (rating_name)
+VALUES ('G');
+
+insert into FILMS (name, rating_id, description, releaseDate, duration)
+values ('name', 1, 'description', '2012-04-22', 200);
+
+CREATE TABLE IF NOT EXISTS FilmsGenres (
+film_id INTEGER REFERENCES FILMS(film_id),
+genre_id INTEGER REFERENCES GENRES(genre_id)
+);
+
+insert into FilmsGenres (film_id, genre_id)
+values (1, 1);
+
+
+
